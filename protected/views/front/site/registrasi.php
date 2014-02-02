992bef25ea23380f7464e02897d62cda576a812d
Arie <arie@wego.com>
2014-02-02 16:54:26 +0700

    update registration from

--- protected/views/front/site/registrasi.php
+++ protected/views/front/site/registrasi.php
@@ -1,53 +1,49 @@
-<div class="container supreme-container">
-	<div class="row">
-		<form class="form-horizontal" action='' method="POST">
-			<fieldset>
-				<div id="legend">
-					<legend class="">Register</legend>
-				</div>
-				<div class="control-group">
-					<!-- Username -->
-					<label class="control-label"  for="username">Username</label>
-					<div class="controls">
-						<input type="text" id="username" name="username" placeholder="" class="input-xlarge">
-						<p class="help-block">Username can contain any letters or numbers, without spaces</p>
-					</div>
-				</div>
-				
-				<div class="control-group">
-					<!-- E-mail -->
-					<label class="control-label" for="email">E-mail</label>
-					<div class="controls">
-						<input type="text" id="email" name="email" placeholder="" class="input-xlarge">
-						<p class="help-block">Please provide your E-mail</p>
-					</div>
-				</div>
-				
-				<div class="control-group">
-					<!-- Password-->
-					<label class="control-label" for="password">Password</label>
-					<div class="controls">
-						<input type="password" id="password" name="password" placeholder="" class="input-xlarge">
-						<p class="help-block">Password should be at least 4 characters</p>
-					</div>
-				</div>
-				
-				<div class="control-group">
-					<!-- Password -->
-					<label class="control-label"  for="password_confirm">Password (Confirm)</label>
-					<div class="controls">
-						<input type="password" id="password_confirm" name="password_confirm" placeholder="" class="input-xlarge">
-						<p class="help-block">Please confirm password</p>
-					</div>
-				</div>
-				
-				<div class="control-group">
-					<!-- Button -->
-					<div class="controls">
-						<button class="btn btn-success">Register</button>
-					</div>
-				</div>
-			</fieldset>
-		</form>
+<div class="container">
+	<div class="row registration">
+		<h3> Just one more step to discover the Advertising Brand. </h3>
+		<hr>
+		<h4>1. Your Basic Information</h4>
+		<div class="col-xs-6 form-row">
+			<label for="exampleInputEmail1">Email address</label>
+			<input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
+		</div>
+		<div class="col-xs-6 form-row">
+			<label for="exampleInputEmail1">Email address</label>
+			<input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
+		</div>
+		<h4>2. Your account credentials</h4>
+		<div class="col-xs-6 form-row">
+			<label for="exampleInputEmail1">Email address</label>
+			<input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
+		</div>
+		<div class="col-xs-6 form-row">
+			<label for="exampleInputEmail1">Email address</label>
+			<input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
+		</div>
+		<div class="col-xs-12 form-row">
+			<label for="exampleInputEmail1">Company Name</label>
+			<input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
+		</div>
+		<h4>3. Your Company Information</h4>
+		<div class="col-xs-6 form-row">
+			<label for="exampleInputEmail1">Email address</label>
+			<input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
+		</div>
+		<div class="col-xs-6 form-row">
+			<label for="exampleInputEmail1">Email address</label>
+			<input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
+		</div>
+		<div class="col-xs-12 form-row">
+			<label for="exampleInputEmail1">Company Name</label>
+			<input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
+		</div>
+		<div class="col-xs-12 form-row">
+			<label for="exampleInputEmail1">Company Name</label>
+			<input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
+		</div>
+		<div class="col-xs-12 form-row">
+			<label for="exampleInputEmail1">Company Name</label>
+			<input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
+		</div>
 	</div>
 </div> <!-- /container -->
