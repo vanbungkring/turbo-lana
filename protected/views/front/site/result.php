c7d4ce3d5ba008cec6871ce13dff522b0f06260c
Arie <arie@wego.com>
2014-02-03 16:20:04 +0700

    update result

--- protected/views/front/site/result.php
+++ protected/views/front/site/result.php
@@ -1,32 +1,24 @@
 <!-- Fixed navbar -->
 <div class="navbar navbar-default navbar-fixed-top" role="navigation">
 
-  <div class="container container-full">
+  <div class="container">
 
-    <div class="navbar-header">
-      <a class="navbar-brand" href="#">Kiviads</a>
+    <div class="row search-header">
+      <div class="btn-group left">
+        <button type="button" class="btn btn-default">List View</button>
+        <button type="button" class="btn btn-default active">Map View</button>
+      </div>
     </div>
-
-    <div class="search-section">
-      <div class="col-md-4">
-        <input type="text" placeholder="Enter email" id="boxcari" class="form-control">
-      </div>
-      <div class="col-md-3">
-        <input type="text" class="form-control" placeholder="date" id="startPicker">
-      </div>
-      <div class="col-md-3">
-        <input type="text" class="form-control" placeholder="type your location" id="endPicker">
-      </div>
-      <div class="col-md-2">
-        <button type="button" class="btn btn-default">Search</button>
-      </div>
+    
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#"><img src="http://www.kiviads.net/img/icn_logo.png"></a>
     </div>
 
     <div class="navbar-collapse collapse">
       <ul class="nav navbar-nav navbar-right">
-        <li><a href="../navbar/">Default</a></li>
-        <li><a href="../navbar-static-top/">Static top</a></li>
-        <li class="active"><a href="./">Fixed top</a></li>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About Us</a></li>
+        <li class="active"><a href="./">Job</a></li>
       </ul>
     </div><!--/.nav-collapse -->
   </div>
@@ -87,131 +79,94 @@
       <div class="modal-body">
         <img src="http://www.kiviads.net/img/billboard-img.jpg">
         <hr>
-        <div class="billboard-explain">
-          <h4>Overflowing text to show scroll behavior</h4>
-          <p>Overflowing text to show scroll behavior
-            Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Donec sed odio dui. Donec ullamcorper nulla non metus auctor fringilla. Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.</p>
-          </div>
-          <div class="billboard-spec">
-            <table class="table table-bordered">
-              <thead>
-                <tr>
-                  <th>#</th>
-                  <th>First Name</th>
-                  <th>Last Name</th>
-                  <th>Username</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td rowspan="2">1</td>
-                  <td>Mark</td>
-                  <td>Otto</td>
-                  <td>@mdo</td>
-                </tr>
-                <tr>
-                  <td>Mark</td>
-                  <td>Otto</td>
-                  <td>@TwBootstrap</td>
-                </tr>
-                <tr>
-                  <td>2</td>
-                  <td>Jacob</td>
-                  <td>Thornton</td>
-                  <td>@fat</td>
-                </tr>
-                <tr>
-                  <td>3</td>
-                  <td colspan="2">Larry the Bird</td>
-                  <td>@twitter</td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
+        <h4>Overflowing text to show scroll behavior</h4>
+        <p>Overflowing text to show scroll behavior s lacus vel augue laoreet rutrum faucibus dolor auctor. Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Donec sed odio dui. Donec ullamcorper nulla non metus auctor fringilla. Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.</p>
+        <div class="billboard-spec">
         </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-          <button type="button" class="btn btn-primary">Save changes</button>
-        </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
       </div>
     </div>
   </div>
+</div>
 
 <?php
 //google maps render
 $js =    '
-    var markers = {};
-    var map = null;
-    var markerCluster = null;
-    $("#map-wrapper").gmap3({
-            map:{
-                options:{
-                        center:[48.8620722, 2.352047],
-                        zoom: 13,
-                        mapTypeId:google.maps.MapTypeId.ROADMAP,
-                        mapTypeControl: false,
-                        navigationControl: false,
-                        scrollwheel: true,
-                        streetViewControl: false
-                },
-                events : {
-                    "idle" : function(){
-                        if(map == null){
-                            map = $(this).gmap3("get");
-                            markerCluster = new MarkerClusterer(map, []);
-                        }
+var markers = {};
+var map = null;
+var markerCluster = null;
+$("#map-wrapper").gmap3({
+  map:{
+    options:{
+      center:[48.8620722, 2.352047],
+      zoom: 13,
+      mapTypeId:google.maps.MapTypeId.ROADMAP,
+      mapTypeControl: false,
+      navigationControl: false,
+      scrollwheel: true,
+      streetViewControl: false
+    },
+    events : {
+      "idle" : function(){
+        if(map == null){
+          map = $(this).gmap3("get");
+          markerCluster = new MarkerClusterer(map, []);
+        }
                         // first get the map bounds
-                        var bounds = map.getBounds();
-                        console.log(bounds);
-                        var url = "'.Yii::app()->createUrl('site/getMarker').'";
-                        $.get(url,{ "bounds" : {
-                            ia_b : bounds.ia.b,
-                            ia_d : bounds.ia.d,
-                            ta_d : bounds.ta.d,
-                            ta_b : bounds.ta.b,
-                        }  },function(retJson){
-                            var newMarker = [];
-                            retJson.forEach(function(row) {                           
-                                if(!(row.id in markers)){
-                                    markers[row.id] = row;
-                                    var latLng = new google.maps.LatLng(row.lat,row.long);
-                                    var marker = new google.maps.Marker({
-                                        position: latLng
-                                    });
-                                    google.maps.event.addListener(marker, "click", function() {
-                                        $("#billboard-popup").modal("show");
-                                    });
-                                    google.maps.event.addListener(marker, "mouseover", function() {
-                                            var infowindow = $("#map-wrapper").gmap3({get:{name:"infowindow"}});
-                                            if (infowindow){
-                                                    infowindow.open(map, marker);
-                                                    infowindow.setContent(row.nama);
-                                            } else {
-                                                    $("#map-wrapper").gmap3({
-                                                            infowindow:{
-                                                                    anchor:marker, 
-                                                                    options:{content: row.nama}
-                                                            }
-                                                    });
-                                            }
-                                    });
+        var bounds = map.getBounds();
+        console.log(bounds);
+        var url = "'.Yii::app()->createUrl('site/getMarker').'";
+        $.get(url,{ "bounds" : {
+          ia_b : bounds.ia.b,
+          ia_d : bounds.ia.d,
+          ta_d : bounds.ta.d,
+          ta_b : bounds.ta.b,
+        }  },function(retJson){
+          var newMarker = [];
+          retJson.forEach(function(row) {                           
+            if(!(row.id in markers)){
+              markers[row.id] = row;
+              var latLng = new google.maps.LatLng(row.lat,row.long);
+              var marker = new google.maps.Marker({
+                position: latLng
+              });
+google.maps.event.addListener(marker, "click", function() {
+  $("#billboard-popup").modal("show");
+});
+google.maps.event.addListener(marker, "mouseover", function() {
+  var infowindow = $("#map-wrapper").gmap3({get:{name:"infowindow"}});
+  if (infowindow){
+    infowindow.open(map, marker);
+    infowindow.setContent(row.nama);
+  } else {
+    $("#map-wrapper").gmap3({
+      infowindow:{
+        anchor:marker, 
+        options:{content: row.nama}
+      }
+    });
+}
+});
 
-                                    newMarker.push(marker);
-                                }
-                                console.log(markers)
-                            });
-                            markerCluster.addMarkers(newMarker);
-                        },"json");
-                    }
-                }
-            },
-            trafficlayer:{
-            },
-            });
-            if(map == null){
-                map = $("#map-wrapper").gmap3("get");
-                markerCluster = new MarkerClusterer(map, []);
-            }
+newMarker.push(marker);
+}
+console.log(markers)
+});
+markerCluster.addMarkers(newMarker);
+},"json");
+}
+}
+},
+trafficlayer:{
+},
+});
+if(map == null){
+  map = $("#map-wrapper").gmap3("get");
+  markerCluster = new MarkerClusterer(map, []);
+}
 ';
 Yii::app()->clientScript->registerScriptFile(Yii::app()->baseUrl.'/js/markerclusterer.js',  CClientScript::POS_END);
 Yii::app()->clientScript->registerScript('script-map',$js,  CClientScript::POS_END);
