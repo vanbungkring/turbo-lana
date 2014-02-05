df0c22d7f0cdaf9d8b12a8e2caa74c12e6593b4e
Arie <arie@wego.com>
2014-02-05 21:01:12 +0700

    add quotation controller

--- protected/config/back.php
+++ protected/config/back.php
@@ -7,7 +7,7 @@ return CMap::mergeArray(
 		'components'=>array(
 			'urlManager'=>array(
 				'urlFormat'=>'path',
-				'rules'=>array( 
+				'rules'=>array(
 					'<controller:\w+>/<id:\d+>'=>'<controller>/view',
 					'<controller:\w+>/<action:\w+>/<id:\d+>'=>'<controller>/<action>',
 					'<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
