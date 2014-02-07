1b9aba35b355807ff10dff6f0234736810989be2
Arie <arie@wego.com>
2014-02-07 20:34:43 +0700

    update index

--- protected/views/front/site/index.php
+++ protected/views/front/site/index.php
@@ -1,55 +1,55 @@
 
-  <body data-spy="scroll" data-offset="0" data-target="#navigation">
+<body data-spy="scroll" data-offset="0" data-target="#navigation">
 
-    <!-- Fixed navbar -->
-	    <div id="navigation" class="navbar navbar-default navbar-fixed-top">
-	      <div class="container">
-	        <div class="navbar-header">
-	          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-	            <span class="icon-bar"></span>
-	            <span class="icon-bar"></span>
-	            <span class="icon-bar"></span>
-	          </button>
-	          <a class="navbar-brand" href="#"><b>Kiviads</b></a>
-	        </div>
-	        <div class="navbar-collapse collapse">
-	          <ul class="nav navbar-nav">
-	            <li class="active"><a href="#home" class="smothscroll">Home</a></li>
-	            <li><a href="#desc" class="smothscroll">Description</a></li>
-	            <li><a href="#showcase" class="smothScroll">Showcase</a></li>
-	            <li><a href="#contact" class="smothScroll">Contact</a></li>
-	          </ul>
-	        </div><!--/.nav-collapse -->
-	      </div>
-	    </div>
+	<!-- Fixed navbar -->
+	<div id="navigation" class="navbar navbar-default navbar-fixed-top">
+		<div class="container">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" href="#"><b>Kiviads</b></a>
+			</div>
+			<div class="navbar-collapse collapse">
+				<ul class="nav navbar-nav">
+					<li class="active"><a href="#home" class="smothscroll">Home</a></li>
+					<li><a href="#desc" class="smothscroll">Description</a></li>
+					<li><a href="#showcase" class="smothScroll">Showcase</a></li>
+					<li><a href="#contact" class="smothScroll">Contact</a></li>
+				</ul>
+			</div><!--/.nav-collapse -->
+		</div>
+	</div>
 
 
 	<section id="home" name="home"></section>
 	<div id="headerwrap">
-	    <div class="container">
-	    	<div class="row centered">
-	    		<div class="col-lg-12">
+		<div class="container">
+			<div class="row centered">
+				<div class="col-lg-12">
 					<h1>Welcome To <b>KiviAds</b></h1>
 					<h3>Show your product with this handsome theme.</h3>
 					<br>
-	    		</div>
-	    		
-	    		<div class="col-lg-2">
-	    			<h5>Amazing Results</h5>
-	    			<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
-	    			<img class="hidden-xs hidden-sm hidden-md" src="assets/img/arrow1.png">
-	    		</div>
-	    		<div class="col-lg-8">
-	    			<img class="img-responsive" src="assets/img/app-bg.png" alt="">
-	    		</div>
-	    		<div class="col-lg-2">
-	    			<br>
-	    			<img class="hidden-xs hidden-sm hidden-md" src="assets/img/arrow2.png">
-	    			<h5>Awesome Design</h5>
-	    			<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
-	    		</div>
-	    	</div>
-	    </div> <!--/ .container -->
+				</div>
+				
+				<div class="col-lg-2">
+					<h5>Amazing Results</h5>
+					<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+					<img class="hidden-xs hidden-sm hidden-md" src="http://www.blacktie.co/demo/pratt/assets/img/arrow1.png">
+				</div>
+				<div class="col-lg-8">
+					<img class="img-responsive" src="http://www.blacktie.co/demo/pratt/assets/img/app-bg.png" alt="">
+				</div>
+				<div class="col-lg-2">
+					<br>
+					<img class="hidden-xs hidden-sm hidden-md" src="http://www.blacktie.co/demo/pratt/assets/img/arrow2.png">
+					<h5>Awesome Design</h5>
+					<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+				</div>
+			</div>
+		</div> <!--/ .container -->
 	</div><!--/ #headerwrap -->
 
 
@@ -62,24 +62,24 @@
 				<br>
 				<br>
 				<div class="col-lg-4">
-					<img src="assets/img/intro01.png" alt="">
+					<img src="http://www.blacktie.co/demo/pratt/assets/img/intro01.png" alt="">
 					<h3>Community</h3>
 					<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
 				</div>
 				<div class="col-lg-4">
-					<img src="assets/img/intro02.png" alt="">
+					<img src="http://www.blacktie.co/demo/pratt/assets/img/intro02.png" alt="">
 					<h3>Schedule</h3>
 					<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
 				</div>
 				<div class="col-lg-4">
-					<img src="assets/img/intro03.png" alt="">
+					<img src="http://www.blacktie.co/demo/pratt/assets/img/intro03.png" alt="">
 					<h3>Monitoring</h3>
 					<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
 				</div>
 			</div>
 			<br>
 			<hr>
-	    </div> <!--/ .container -->
+		</div> <!--/ .container -->
 	</div><!--/ #introwrap -->
 	
 	<!-- FEATURES WRAP -->
@@ -90,69 +90,69 @@
 				<br>
 				<br>
 				<div class="col-lg-6 centered">
-					<img class="centered" src="assets/img/mobile.png" alt="">
+					<img class="centered" src="http://www.blacktie.co/demo/pratt/assets/img/mobile.png" alt="">
 				</div>
 				
 				<div class="col-lg-6">
 					<h3>Some Features</h3>
 					<br>
-				<!-- ACCORDION -->
-		            <div class="accordion ac" id="accordion2">
-		                <div class="accordion-group">
-		                    <div class="accordion-heading">
-		                        <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#collapseOne">
-		                        First Class Design
-		                        </a>
-		                    </div><!-- /accordion-heading -->
-		                    <div id="collapseOne" class="accordion-body collapse in">
-		                        <div class="accordion-inner">
-								<p>It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+					<!-- ACCORDION -->
+					<div class="accordion ac" id="accordion2">
+						<div class="accordion-group">
+							<div class="accordion-heading">
+								<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#collapseOne">
+									First Class Design
+								</a>
+							</div><!-- /accordion-heading -->
+							<div id="collapseOne" class="accordion-body collapse in">
+								<div class="accordion-inner">
+									<p>It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
 								</div><!-- /accordion-inner -->
-		                    </div><!-- /collapse -->
-		                </div><!-- /accordion-group -->
-		                <br>
-		
-		                <div class="accordion-group">
-		                    <div class="accordion-heading">
-		                        <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#collapseTwo">
-		                        Retina Ready Theme
-		                        </a>
-		                    </div>
-		                    <div id="collapseTwo" class="accordion-body collapse">
-		                        <div class="accordion-inner">
-								<p>It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+							</div><!-- /collapse -->
+						</div><!-- /accordion-group -->
+						<br>
+						
+						<div class="accordion-group">
+							<div class="accordion-heading">
+								<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#collapseTwo">
+									Retina Ready Theme
+								</a>
+							</div>
+							<div id="collapseTwo" class="accordion-body collapse">
+								<div class="accordion-inner">
+									<p>It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
 								</div><!-- /accordion-inner -->
-		                    </div><!-- /collapse -->
-		                </div><!-- /accordion-group -->
-		                <br>
-		
-		                 <div class="accordion-group">
-		                    <div class="accordion-heading">
-		                        <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#collapseThree">
-		                        Awesome Support
-		                        </a>
-		                    </div>
-		                    <div id="collapseThree" class="accordion-body collapse">
-		                        <div class="accordion-inner">
-								<p>It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+							</div><!-- /collapse -->
+						</div><!-- /accordion-group -->
+						<br>
+						
+						<div class="accordion-group">
+							<div class="accordion-heading">
+								<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#collapseThree">
+									Awesome Support
+								</a>
+							</div>
+							<div id="collapseThree" class="accordion-body collapse">
+								<div class="accordion-inner">
+									<p>It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
 								</div><!-- /accordion-inner -->
-		                    </div><!-- /collapse -->
-		                </div><!-- /accordion-group -->
-		                <br>
-		                
-		                 <div class="accordion-group">
-		                    <div class="accordion-heading">
-		                        <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#collapseFour">
-		                        Responsive Design
-		                        </a>
-		                    </div>
-		                    <div id="collapseFour" class="accordion-body collapse">
-		                        <div class="accordion-inner">
-								<p>It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+							</div><!-- /collapse -->
+						</div><!-- /accordion-group -->
+						<br>
+						
+						<div class="accordion-group">
+							<div class="accordion-heading">
+								<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#collapseFour">
+									Responsive Design
+								</a>
+							</div>
+							<div id="collapseFour" class="accordion-body collapse">
+								<div class="accordion-inner">
+									<p>It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
 								</div><!-- /accordion-inner -->
-		                    </div><!-- /collapse -->
-		                </div><!-- /accordion-group -->
-		                <br>			
+							</div><!-- /collapse -->
+						</div><!-- /accordion-group -->
+						<br>			
 					</div><!-- Accordion -->
 				</div>
 			</div>
@@ -168,21 +168,21 @@
 				<br>
 				<div class="col-lg-8 col-lg-offset-2">
 					<div id="carousel-example-generic" class="carousel slide">
-					  <!-- Indicators -->
-					  <ol class="carousel-indicators">
-					    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-					    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-					  </ol>
-					
-					  <!-- Wrapper for slides -->
-					  <div class="carousel-inner">
-					    <div class="item active">
-					      <img src="assets/img/item-01.png" alt="">
-					    </div>
-					    <div class="item">
-					      <img src="assets/img/item-02.png" alt="">
-					    </div>
-					  </div>
+						<!-- Indicators -->
+						<ol class="carousel-indicators">
+							<li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+							<li data-target="#carousel-example-generic" data-slide-to="1"></li>
+						</ol>
+						
+						<!-- Wrapper for slides -->
+						<div class="carousel-inner">
+							<div class="item active">
+								<img src="http://www.blacktie.co/demo/pratt/assets/img/item-01.png" alt="">
+							</div>
+							<div class="item">
+								<img src="http://www.blacktie.co/demo/pratt/assets/img/item-02.png" alt="">
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>
@@ -199,10 +199,10 @@
 			<div class="col-lg-5">
 				<h3>Address</h3>
 				<p>
-				Av. Greenville 987,<br/>
-				New York,<br/>
-				90873<br/>
-				United States
+					Av. Greenville 987,<br/>
+					New York,<br/>
+					90873<br/>
+					United States
 				</p>
 			</div>
 			
@@ -210,28 +210,28 @@
 				<h3>Drop Us A Line</h3>
 				<br>
 				<form role="form" action="#" method="post" enctype="plain"> 
-				  <div class="form-group">
-				    <label for="name1">Your Name</label>
-				    <input type="name" name="Name" class="form-control" id="name1" placeholder="Your Name">
-				  </div>
-				  <div class="form-group">
-				    <label for="email1">Email address</label>
-				    <input type="email" name="Mail" class="form-control" id="email1" placeholder="Enter email">
-				  </div>
-				  <div class="form-group">
-				  	<label>Your Text</label>
-				  	<textarea class="form-control" name="Message" rows="3"></textarea>
-				  </div>
-				  <br>
-				  <button type="submit" class="btn btn-large btn-success">SUBMIT</button>
+					<div class="form-group">
+						<label for="name1">Your Name</label>
+						<input type="name" name="Name" class="form-control" id="name1" placeholder="Your Name">
+					</div>
+					<div class="form-group">
+						<label for="email1">Email address</label>
+						<input type="email" name="Mail" class="form-control" id="email1" placeholder="Enter email">
+					</div>
+					<div class="form-group">
+						<label>Your Text</label>
+						<textarea class="form-control" name="Message" rows="3"></textarea>
+					</div>
+					<br>
+					<button type="submit" class="btn btn-large btn-success">SUBMIT</button>
 				</form>
 			</div>
 		</div>
 	</div>
-<div id="c">
-    <div class="container">
-      <p>Created by <a href="http://www.blacktie.co">BLACKTIE.CO</a></p>
-    
-    </div>
-  </div>
-  
+	<div id="c">
+		<div class="container">
+			<p>Created by <a href="http://www.blacktie.co">BLACKTIE.CO</a></p>
+			
+		</div>
+	</div>
+	
