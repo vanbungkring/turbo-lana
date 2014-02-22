54e1063be04b23f9f88b0f894922277bd2db46d5
Arie <arie@wego.com>
2014-02-22 21:13:21 +0700

    remove unused file

--- protected/views/front/user/user-dashboard.php
+++ protected/views/front/user/user-dashboard.php
@@ -9,6 +9,12 @@
 				</div>
 
 				<div class="navbar-collapse collapse">
+					<ul class="nav navbar-nav">
+					<li class="active"><a href="#home" class="smothscroll">Home</a></li>
+					<li><a href="#desc" class="smothscroll">Description</a></li>
+					<li><a href="#showcase" class="smothScroll">Showcase</a></li>
+					<li><a href="#contact" class="smothScroll">Contact</a></li>
+				</ul>
 					<ul class="nav navbar-nav navbar-right">
 						<li><a href="#">Home</a></li>
 						<li><a href="#">About Us</a></li>
