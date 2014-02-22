f5d090925f03c7da4c25d6c125a36966a20d908e
Arie <arie@wego.com>
2014-02-22 21:15:17 +0700

    update user dashboard menu

--- protected/views/front/user/user-dashboard.php
+++ protected/views/front/user/user-dashboard.php
@@ -10,15 +10,11 @@
 
 				<div class="navbar-collapse collapse">
 					<ul class="nav navbar-nav">
-					<li class="active"><a href="#home" class="smothscroll">Home</a></li>
-					<li><a href="#desc" class="smothscroll">Description</a></li>
-					<li><a href="#showcase" class="smothScroll">Showcase</a></li>
-					<li><a href="#contact" class="smothScroll">Contact</a></li>
+					<li><a href="#desc" class="smothscroll">Dashboard</a></li>
+					<li><a href="#desc" class="smothscroll">My Bookmark</a></li>
+					<li><a href="#showcase" class="smothScroll">RFP</a></li>
 				</ul>
 					<ul class="nav navbar-nav navbar-right">
-						<li><a href="#">Home</a></li>
-						<li><a href="#">About Us</a></li>
-						<li><a href="./">Maps</a></li>
 						<?php 
 						if(!Yii::app()->user->isGuest) {
 							echo  '<li><a href="'.Yii::app()->createUrl('site/logout').'">Logout</a></li>';
