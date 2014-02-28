797125135e6423df195dbc0a10cd6cdc668a0380
lateph <lateph@gmail.com>
2014-02-28 20:04:18 +0700

    bug fix link create quote

--- protected/views/front/rfp/list.php
+++ protected/views/front/rfp/list.php
@@ -11,7 +11,7 @@
     <div class="col-lg-12">
         <div class="panel panel-default">
             <div class="panel-body">
-                <a href="<?php echo Yii::app()->createUrl('/rfp/list'); ?>" >Create Quote</a>
+                <a href="<?php echo Yii::app()->createUrl('/rfp/'); ?>" >Create Quote</a>
                 <?php $this->widget('zii.widgets.grid.CGridView', array(
                         'id'=>'banner-grid',
                         "itemsCssClass" => 'table',
