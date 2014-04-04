0fcefa0a91509e4c727e78d8efea0d5ba6e54814
Arie <arie@wego.com>
2014-04-05 00:20:11 +0700

    update intro into partial

---
+++ protected/views/front/shared/partial/homepage/city.php
@@ -0,0 +1,39 @@
+	<div class="city-billboard">
+	<div class="container">
+		<div class="row">
+			<h4>Featured Cities</h4>
+				<div class="col-sm-10 col-md-offset-1">
+              		<div class="col-sm-4">
+	              		<div class="panel panel-default">
+		                	<div class="panel-body"><a href="/bootstrap-template/basic"> <img src="<?= Yii::app()->getBaseUrl(true) ?>/images/jakarta.jpg" alt="Jakarta" class="img-responsive"></a></div>
+                		</div> 
+                	</div>    
+                	<div class="col-sm-4">
+	              		<div class="panel panel-default">
+		                	<div class="panel-body"><a href="/bootstrap-template/basic"> <img src="<?= Yii::app()->getBaseUrl(true) ?>/images/bandung.jpg" alt="Jakarta" class="img-responsive"></a></div>
+                		</div> 
+                	</div> 
+                	<div class="col-sm-4">
+	              		<div class="panel panel-default">
+		                	<div class="panel-body"><a href="/bootstrap-template/basic"> <img src="<?= Yii::app()->getBaseUrl(true) ?>/images/jakarta.jpg" alt="Jakarta" class="img-responsive"></a></div>
+                		</div> 
+                	</div> 
+                	<div class="col-sm-4">
+	              		<div class="panel panel-default">
+		                	<div class="panel-body"><a href="/bootstrap-template/basic"> <img src="<?= Yii::app()->getBaseUrl(true) ?>/images/jakarta.jpg" alt="Jakarta" class="img-responsive"></a></div>
+                		</div> 
+                	</div> 
+                	<div class="col-sm-4">
+	              		<div class="panel panel-default">
+		                	<div class="panel-body"><a href="/bootstrap-template/basic"> <img src="<?= Yii::app()->getBaseUrl(true) ?>/images/jakarta.jpg" alt="Jakarta" class="img-responsive"></a></div>
+                		</div> 
+                	</div> 
+                	<div class="col-sm-4">
+	              		<div class="panel panel-default">
+		                	<div class="panel-body"><a href="/bootstrap-template/basic"> <img src="<?= Yii::app()->getBaseUrl(true) ?>/images/jakarta.jpg" alt="Jakarta" class="img-responsive"></a></div>
+                		</div> 
+                	</div>          
+      			</div>	            
+		</div>
+	</div><!--/ .container -->
+</div>
