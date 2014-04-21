ce67831ab788ab95e034e968b14ce686a619d89b
Arie <Bukik@Bukik-MacBook.local>
2014-04-21 13:05:53 +0700

    update gitignore

--- protected/views/front/site/login.php
+++ protected/views/front/site/login.php
@@ -1,58 +1,78 @@
-    <?php $this->renderPartial('/shared/partial/navbar'); ?>
-    <?php
-    /* @var $this SiteController */
-    /* @var $model LoginForm */
-    /* @var $form CActiveForm  */
+<?php
 
-    $this->pageTitle=Yii::app()->name . ' - Login';
-    $this->breadcrumbs=array(
-    	'Login',
-    	);
-    	?>
-    	<div class="container kivi">
-    		<div class="row">
-    			<h1>Login</h1>
+  $this->pageTitle=Yii::app()->name . ' - Login';
+  $this->breadcrumbs=array('Login',);
 
-    			<p>Please fill out the following form with your login credentials:</p>
+?>
+<div class="container big-login">
+  <div class="row">
+       <div class="col-md-6 col-md-offset-3">
+        
+        <h1 id="signin_logo" class="login-sprite sprout-logo hide-text">
+          <a href="http://sproutsocial.com ">Sprout Social</a> 
+        </h1>
+       
+       </div>
+  </div>
+</div>
 
-    			<div class="form">
-    				<?php $form=$this->beginWidget('CActiveForm', array(
-    					'id'=>'login-form',
-    					'enableClientValidation'=>true,
-    					'clientOptions'=>array(
-    						'validateOnSubmit'=>true,
-    						),
-    						)); ?>
+      <?php
+      /* @var $this SiteController */
+      /* @var $model LoginForm */
+      /* @var $form CActiveForm  */
 
-    						<p class="note">Fields with <span class="required">*</span> are required.</p>
+      $this->pageTitle=Yii::app()->name . ' - Login';
+      $this->breadcrumbs=array(
+       'Login',
+       );
+       ?>
+       <div class="container kivi">
+        <div class="row">
 
-    						<div class="row">
-    							<?php echo $form->labelEx($model,'username'); ?>
-    							<?php echo $form->textField($model,'username'); ?>
-    							<?php echo $form->error($model,'username'); ?>
-    						</div>
+         <h1>Login</h1>
 
-    						<div class="row">
-    							<?php echo $form->labelEx($model,'password'); ?>
-    							<?php echo $form->passwordField($model,'password'); ?>
-    							<?php echo $form->error($model,'password'); ?>
-    							<p class="hint">
-    								Hint: You may login with <kbd>demo</kbd>/<kbd>demo</kbd> or <kbd>admin</kbd>/<kbd>admin</kbd>.
-    							</p>
-    						</div>
+         <p>Please fill out the following form with your login credentials:</p>
 
-    						<div class="row rememberMe">
-    							<?php echo $form->checkBox($model,'rememberMe'); ?>
-    							<?php echo $form->label($model,'rememberMe'); ?>
-    							<?php echo $form->error($model,'rememberMe'); ?>
-    						</div>
+         <div class="form">
+          <?php $form=$this->beginWidget('CActiveForm', array(
+           'id'=>'login-form',
+           'enableClientValidation'=>true,
+           'clientOptions'=>array(
+            'validateOnSubmit'=>true,
+            ),
+            )); ?>
 
-    						<div class="row buttons">
-    							<?php echo CHtml::submitButton('Login'); ?>
-                               
-    						</div>
+            <p class="note">Fields with <span class="required">*</span> are required.</p>
 
-    						<?php $this->endWidget(); ?>
-    					</div><!-- form -->
-    				</div>
-    			</div> <?php $this->widget('ext.hoauth.widgets.HOAuth'); ?>
+            <div class="row">
+             <?php echo $form->labelEx($model,'username'); ?>
+             <?php echo $form->textField($model,'username'); ?>
+             <?php echo $form->error($model,'username'); ?>
+           </div>
+
+           <div class="row">
+             <?php echo $form->labelEx($model,'password'); ?>
+             <?php echo $form->passwordField($model,'password'); ?>
+             <?php echo $form->error($model,'password'); ?>
+             <p class="hint">
+              Hint: You may login with <kbd>demo</kbd>/<kbd>demo</kbd> or <kbd>admin</kbd>/<kbd>admin</kbd>.
+            </p>
+          </div>
+
+          <div class="row rememberMe">
+           <?php echo $form->checkBox($model,'rememberMe'); ?>
+           <?php echo $form->label($model,'rememberMe'); ?>
+           <?php echo $form->error($model,'rememberMe'); ?>
+         </div>
+
+         <div class="row buttons">
+           <?php echo CHtml::submitButton('Login'); ?>
+
+         </div>
+
+         <?php $this->endWidget(); ?>
+       </div><!-- form -->
+     </div>
+   </div>
+
+   <?php $this->widget('ext.hoauth.widgets.HOAuth'); ?>
