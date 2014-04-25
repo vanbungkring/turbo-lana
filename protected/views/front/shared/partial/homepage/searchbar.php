d77f23f405cadc6786968a5abcedcdfcfed4bc8a
Arie Prasetyo <arieprasetyo@Aries-MacBook.local>
2014-04-25 22:15:23 +0700

    update front detapn

--- protected/views/front/shared/partial/homepage/searchbar.php
+++ protected/views/front/shared/partial/homepage/searchbar.php
@@ -1,10 +1,18 @@
+<div class="search-box">
+<h1>Flight search. Reinvented for humans.</h1>
 
 <form action="<?php echo Yii::app()->createUrl('site/result'); ?>" method="GET">
 	<div class="row row-search">
-		<div class="col-xs-4 free-transform">
-			<input type="text" class="form-control search-form" placeholder="Search Location" id="boxcari" autocomplete="off">
+		<div class="col-xs-6 free-transform">
+			<input type="text" class="form-control search-form " placeholder="Search Location" id="boxcari" autocomplete="off">
 			<input type="hidden" id="lat" value="-6.17511" name="lat">
 			<input type="hidden" id="long" value="106.86503949999997" name="long">
+		</div>
+		<div class="col-xs-2 free-transform">
+			<input type="text" class="form-control search-form hasDatepicker_start" placeholder="Date Start" autocomplete="off">
+		</div>
+		<div class="col-xs-2 free-transform">
+			<input type="text" class="form-control search-form hasDatepicker_end" placeholder="Date End" autocomplete="off">
 		</div>
 
 		<div class="col-xs-1 free-transform">
@@ -13,3 +21,5 @@
 		<div class="check"></div>
 	</div>
 </form>
+<p>Search 12808 Media Spaces in 352 locations.</p>
+</div>
