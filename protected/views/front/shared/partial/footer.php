9b8cb6ec10f2c882796a51962469d578749c6dd9
Arie Prasetyo <prasetyo.mailbox@gmail.com>
2014-05-02 21:42:28 +0700

    update data

---
+++ protected/views/front/shared/partial/footer.php
@@ -0,0 +1,40 @@
+<div class="footerwrap">
+    <div class="container">
+      <div class="col-md-3">
+        <h3>Address</h3>
+        <p>
+        Av. Greenville 987,<br>
+        New York,<br>
+        90873<br>
+        United States
+        </p>
+      </div>
+      
+      <div class="col-lg-3">
+        <h3>Drop Us A Line</h3>
+        <br>
+        <form role="form" action="#" method="post" enctype="plain"> 
+          <div class="form-group">
+            <label for="name1">Your Name</label>
+            <input type="name" name="Name" class="form-control" id="name1" placeholder="Your Name">
+          </div>
+          <div class="form-group">
+            <label for="email1">Email address</label>
+            <input type="email" name="Mail" class="form-control" id="email1" placeholder="Enter email">
+          </div>
+          <div class="form-group">
+            <label>Your Text</label>
+            <textarea class="form-control" name="Message" rows="3"></textarea>
+          </div>
+          <br>
+          <button type="submit" class="btn btn-large btn-success">SUBMIT</button>
+        </form>
+      </div>
+    </div>
+  </div>
+  <div class="bottom-line">
+    <div class="container">
+      <p>Created by <a href="http://www.blacktie.co">BLACKTIE.CO</a></p>
+    
+    </div>
+  </div>
