259aca0a5dcc92fd5c4909183deca4eebe15cea2
Arie Prasetyo <prasetyo.mailbox@gmail.com>
2014-05-02 17:38:53 +0700

    update add faq

--- protected/config/front.php
+++ protected/config/front.php
@@ -12,7 +12,7 @@ return CMap::mergeArray(
 			'urlManager'=>array(
 				'urlFormat'=>'path',
 				'rules'=>array(
-					'<view:(bandung|bekasi|bogor|jakarta|tangerang)>' => 'site/page',
+					'<view:(bandung|bekasi|bogor|jakarta|tangerang|about)>' => 'site/page',
 					'dashboard'=>'user/Dashboard',
 					'register'=>'site/registrasi',
 					'search'=>'site/result',
