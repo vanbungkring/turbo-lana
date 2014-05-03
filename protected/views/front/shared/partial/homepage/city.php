c8eca92c3ad6ef453e56ff5671dc8477734ad073
Arie Prasetyo <prasetyo.mailbox@gmail.com>
2014-05-03 15:02:41 +0700

    update

--- protected/views/front/shared/partial/homepage/city.php
+++ protected/views/front/shared/partial/homepage/city.php
@@ -38,7 +38,7 @@
 					<div class="col-sm-4">
 						<div class="panel panel-default overlayed">
 							<h2>Bali</h2>
-							<div class="panel-body"><a href="<?php echo Yii::app()->createUrl('/bali');?>" title="Bali"> <img src="http://travelphotos.lonelywalks.com/travel-photos-makassar-hasanuddin-ariport-2.jpg" alt="Makassar" class="img-responsive"></a></div>
+							<div class="panel-body"><a href="<?php echo Yii::app()->createUrl('/denpasar');?>" title="Bali"> <img src="http://travelphotos.lonelywalks.com/travel-photos-makassar-hasanuddin-ariport-2.jpg" alt="Makassar" class="img-responsive"></a></div>
 						</div> 
 					</div>         
 				</div>	            
