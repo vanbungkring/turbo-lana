30f58b6f0cc4d8e061470d2315ca8cd2098a50ad
Arie Prasetyo <prasetyo.mailbox@gmail.com>
2014-05-05 22:11:18 +0700

    update footer copywriting

--- protected/views/front/shared/partial/footer.php
+++ protected/views/front/shared/partial/footer.php
@@ -3,7 +3,7 @@
     <div class="col-md-5">
       <h3>Kiviads</h3>
       <p>
-       Saat ini beriklan di billboard dapat dengan cepat meningkatkan kesadaran masyarakan akan brand tersebut. Tidak hanya kesadaran, namun peningkatan angka penjualan pun dapat diraih oleh pemilik brand. Untuk itulah Kiviads ada! Sebuah marketplace dimana pengiklan dapat memilih secara langsung titik-titik papan reklame yang berada di lokasi strategis, serta melakukan kampanye iklan melalui sistem online secara cepat dan mudah.
+       Saat ini beriklan di billboard dapat dengan cepat meningkatkan kesadaran masyarakan akan brand tersebut. Tidak hanya kesadaran, namun peningkatan angka penjualan pun dapat diraih oleh pemilik brand.</p><p> Untuk itulah Kiviads ada! Sebuah marketplace dimana pengiklan dapat memilih secara langsung titik-titik papan reklame yang berada di lokasi strategis, serta melakukan kampanye iklan melalui sistem online secara cepat dan mudah.
       </p>
     </div>
     <div class="col-md-2">
