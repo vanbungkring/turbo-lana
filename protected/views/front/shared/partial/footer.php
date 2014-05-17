7e2c56502c13f6fc577a5845457d4fc2a659b8cc
Arie Prasetyo <prasetyo.mailbox@gmail.com>
2014-05-17 17:41:56 +0700

    update footer

--- protected/views/front/shared/partial/footer.php
+++ protected/views/front/shared/partial/footer.php
@@ -11,8 +11,8 @@
       <ul>
        <li><a href="<?php echo Yii::app()->createUrl('/advertiser');?>">Advertiser</a></li>
        <li><a href="<?php echo Yii::app()->createUrl('/media-owner');?>">Media Owner</a></li>
-       <li><a href="<?php echo Yii::app()->createUrl('/faq');?>">FAQ</a></li>
-       <li><a href="<?php echo Yii::app()->createUrl('/help-center');?>">Help Center</a></li>
+       <!-- <li><a href="<?php echo Yii::app()->createUrl('/faq');?>">FAQ</a></li> -->
+
        <li><a href="<?php echo Yii::app()->createUrl('/contact');?>">Contact US</a></li>
      </ul>
    </div>
@@ -26,23 +26,23 @@
  </div>
 
  <div class="col-md-3">
-  <h3>Drop Us A Line</h3>
+  <h3Tinggalkan Pesan</h3>
   <br>
   <form role="form" action="#" method="post" enctype="plain"> 
     <div class="form-group">
-      <label for="name1">Your Name</label>
-      <input type="name" name="Name" class="form-control" id="name1" placeholder="Your Name">
+      <label for="name1">Nama</label>
+      <input type="name" name="Name" class="form-control" id="name1" placeholder="Nama Anda">
     </div>
     <div class="form-group">
-      <label for="email1">Email address</label>
-      <input type="email" name="Mail" class="form-control" id="email1" placeholder="Enter email">
+      <label for="email1">Email</label>
+      <input type="email" name="Mail" class="form-control" id="email1" placeholder="Email Anda">
     </div>
     <div class="form-group">
-      <label>Your Text</label>
+      <label>Pesan Anda</label>
       <textarea class="form-control" name="Message" rows="3"></textarea>
     </div>
     <br>
-    <button type="submit" class="btn btn-large btn-success">SUBMIT</button>
+    <button type="submit" class="btn btn-large btn-success">Kirim</button>
   </form>
 </div>
 </div>
