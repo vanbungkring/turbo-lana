4ce1c5259058e1e3382e072ba744c717f55d2907
lateph <lataeph@gmail.com>
2014-05-19 13:06:19 +0700

    add email new register

--- protected/config/main.php
+++ protected/config/main.php
@@ -72,10 +72,12 @@ return array(
 		),
 		'mailgun' => array(
             'class' => 'application.extensions.php-mailgun.MailgunYii',
-            'domain' => 'beta.kiviads.net',
+            'domain' => 'kiviads.net',
             'key' => 'key-0wztik6tn-zbm1gkcv1h95jh08m1aiu3',
             'tags' => array(), // You may also specify some Mailgun parameters
             'enableTracking' => false,
+            'fromAddress'=>'support@kiviads.net',
+            'fromName'=>'Suport kiviads',
         ),
 	),
 
---
+++ protected/views/mail/new-user.php
@@ -0,0 +1,187 @@
+<html>
+
+<head>
+<meta http-equiv=Content-Type content="text/html; charset=windows-1252">
+<meta name=Generator content="Microsoft Word 14 (filtered)">
+<style>
+<!--
+ /* Font Definitions */
+ @font-face
+	{font-family:Calibri;
+	panose-1:2 15 5 2 2 2 4 3 2 4;}
+ /* Style Definitions */
+ p.MsoNormal, li.MsoNormal, div.MsoNormal
+	{margin:0in;
+	margin-bottom:.0001pt;
+	font-size:12.0pt;
+	font-family:"Calibri","sans-serif";}
+a:link, span.MsoHyperlink
+	{color:blue;
+	text-decoration:underline;}
+a:visited, span.MsoHyperlinkFollowed
+	{color:purple;
+	text-decoration:underline;}
+p.MsoListParagraph, li.MsoListParagraph, div.MsoListParagraph
+	{margin-top:0in;
+	margin-right:0in;
+	margin-bottom:0in;
+	margin-left:.5in;
+	margin-bottom:.0001pt;
+	font-size:12.0pt;
+	font-family:"Calibri","sans-serif";}
+p.MsoListParagraphCxSpFirst, li.MsoListParagraphCxSpFirst, div.MsoListParagraphCxSpFirst
+	{margin-top:0in;
+	margin-right:0in;
+	margin-bottom:0in;
+	margin-left:.5in;
+	margin-bottom:.0001pt;
+	font-size:12.0pt;
+	font-family:"Calibri","sans-serif";}
+p.MsoListParagraphCxSpMiddle, li.MsoListParagraphCxSpMiddle, div.MsoListParagraphCxSpMiddle
+	{margin-top:0in;
+	margin-right:0in;
+	margin-bottom:0in;
+	margin-left:.5in;
+	margin-bottom:.0001pt;
+	font-size:12.0pt;
+	font-family:"Calibri","sans-serif";}
+p.MsoListParagraphCxSpLast, li.MsoListParagraphCxSpLast, div.MsoListParagraphCxSpLast
+	{margin-top:0in;
+	margin-right:0in;
+	margin-bottom:0in;
+	margin-left:.5in;
+	margin-bottom:.0001pt;
+	font-size:12.0pt;
+	font-family:"Calibri","sans-serif";}
+.MsoChpDefault
+	{font-family:"Calibri","sans-serif";}
+.MsoPapDefault
+	{margin-bottom:10.0pt;
+	line-height:115%;}
+@page WordSection1
+	{size:8.5in 11.0in;
+	margin:1.0in 1.0in 1.0in 1.0in;}
+div.WordSection1
+	{page:WordSection1;}
+ /* List Definitions */
+ ol
+	{margin-bottom:0in;}
+ul
+	{margin-bottom:0in;}
+-->
+</style>
+
+</head>
+
+<body lang=EN-US link=blue vlink=purple>
+
+<div class=WordSection1>
+
+<p class=MsoNormal align=center style='text-align:center;line-height:115%'><b><span
+style='font-size:18.0pt;line-height:115%'>Email Notification Contents for
+User(s)</span></b></p>
+
+<p class=MsoNormal style='text-align:justify;line-height:115%'>&nbsp;</p>
+
+<p class=MsoNormal style='text-align:justify;line-height:115%'>&nbsp;</p>
+
+<p class=MsoNormal style='text-align:justify;line-height:115%'><b><u>1.
+Welcoming Email to New User(s)</u></b></p>
+
+<p class=MsoNormal style='text-align:justify;line-height:115%'>&nbsp;</p>
+
+<p class=MsoNormal align=center style='text-align:center;line-height:115%'>&nbsp;</p>
+
+<p class=MsoNormal style='line-height:115%'>Hi, <?php echo $member->namaDepan; ?>, selamat
+bergabung di Kiviads!</p>
+
+<p class=MsoNormal style='text-align:justify;line-height:115%'>&nbsp;</p>
+
+<p class=MsoNormal style='text-align:justify;line-height:115%'>Terima kasih
+telah mendaftarkan diri Anda di Kiviads. Melalui Kiviads Anda dapat mencari,
+memilih, mengatur, serta memproses semua kebutuhan <i>campaign</i> iklan Anda
+dengan cepat dan mudah.</p>
+
+<p class=MsoNormal style='text-align:justify;line-height:115%'>&nbsp;</p>
+
+<p class=MsoNormal style='text-align:justify;line-height:115%'>Berikut hal-hal
+yang dapat Anda lakukan dengan Kiviads:</p>
+
+<p class=MsoNormal style='text-align:justify;line-height:115%'>&nbsp;</p>
+
+<p class=MsoListParagraphCxSpFirst style='margin-left:.25in;text-align:justify;
+text-indent:-.25in;line-height:115%'>1.<span style='font:7.0pt "Times New Roman"'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+</span><u>Update profil perusahaan Anda</u> <span style='background:yellow'>(Link
+ke http://dev.oddro.com/turbo-lana/index.php/user/profile)</span></p>
+
+<p class=MsoListParagraphCxSpMiddle style='margin-left:.25in;text-align:justify;
+text-indent:-.25in;line-height:115%'>2.<span style='font:7.0pt "Times New Roman"'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+</span><u>Cari titik <i>billboard</i></u> <span style='background:yellow'>(link
+ke </span><a href="http://dev.oddro.com/turbo-lana/index.php/search"><span
+style='background:yellow'>http://dev.oddro.com/turbo-lana/index.php/search</span></a><span
+style='background:yellow'>?)</span> di lebih dari 100 titik yang terdaftar di
+Kiviads </p>
+
+<p class=MsoListParagraphCxSpMiddle style='margin-left:.25in;text-align:justify;
+text-indent:-.25in;line-height:115%'>3.<span style='font:7.0pt "Times New Roman"'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+</span><u>Rencanakan <i>campaign</i> iklan Anda</u> <span style='background:
+yellow'>(link http://dev.oddro.com/turbo-lana/index.php/quote3)</span> dengan
+sempurna</p>
+
+<p class=MsoListParagraphCxSpLast style='margin-left:.25in;text-align:justify;
+line-height:115%'>&nbsp;</p>
+
+<p class=MsoNormal style='text-align:justify;line-height:115%'>Nikmati
+kemudahan serta proses persiapan hingga pelaksanaan <i>campaign</i> yang serba
+cepat!</p>
+
+<p class=MsoNormal style='text-align:justify;line-height:115%'>&nbsp;</p>
+
+<p class=MsoNormal style='text-align:justify;line-height:115%'>Tidak perlu lagi
+membuang waktu untuk pergi <i>meeting</i> dan terjebak kemacetan karena semua
+dapat dilakukan melalui Kiviads, termasuk Anda yang berada diluar Jakarta!</p>
+
+<p class=MsoNormal style='text-align:justify;line-height:115%'>&nbsp;</p>
+
+<p class=MsoNormal style='text-align:justify;line-height:115%'>Semoga hari Anda
+menyenangkan!</p>
+
+<p class=MsoNormal style='text-align:justify;line-height:115%'>&nbsp;</p>
+
+<p class=MsoNormal align=center style='text-align:center;line-height:115%'>(Logo
+Kiviads by Kivibyte)</p>
+
+<p class=MsoNormal style='text-align:justify;line-height:115%'>&nbsp;</p>
+
+<p class=MsoNormal align=center style='text-align:center;line-height:115%'>Follow
+akun Twitter kami: @kivibyte (kasih logo Twitter)</p>
+
+<p class=MsoNormal align=center style='text-align:center;line-height:115%'>Gabung
+di Facebook Fanpage: Kivibyte (kasih logo Facebook)</p>
+
+<p class=MsoNormal style='text-align:justify;line-height:115%'>&nbsp;</p>
+
+<p class=MsoNormal style='text-align:justify;line-height:115%'>&nbsp;</p>
+
+<p class=MsoNormal style='text-align:justify;line-height:115%'>--</p>
+
+<p class=MsoNormal style='text-align:justify;line-height:115%'>&nbsp;</p>
+
+<p class=MsoNormal style='text-align:justify;line-height:115%'>Apabila Anda
+memerlukan bantuan, jangan ragu untuk menghubungi kami di:</p>
+
+<p class=MsoNormal style='text-align:justify;line-height:115%'>&nbsp;</p>
+
+<p class=MsoNormal style='text-align:justify;line-height:115%'>Telp: 021 71 29
+29 73</p>
+
+<p class=MsoNormal style='text-align:justify;line-height:115%'>Email:
+support@kiviads.net </p>
+
+<p class=MsoNormal>&nbsp;</p>
+
+</div>
+
+</body>
+
+</html>
