67e26c65a81abbdf45bf281c391992c40278026f
Arie Prasetyo <prasetyo.mailbox@gmail.com>
2014-06-17 18:30:39 +0700

    update netra, update fq list

--- protected/views/front/site/pages/faq.php
+++ protected/views/front/site/pages/faq.php
@@ -1,8 +1,95 @@
 <?php $this->
 renderPartial('/shared/partial/navbar'); ?>
 <div class="container kivi city">
-	
-</div>
-<!-- end container -->
-<?php $this->
-renderPartial('/shared/partial/footer');?>
+
+  <h1 class="entry-title">Frequently Asked Questions</h1>
+  <div class="col-md-8">
+    <div class="entry-content" id="gig-div-buttons-392-top_gig_containerParent">
+      <article id="1">
+        <h3><strong>Apa keunggulan Kiviads?</strong></h3>
+        <ul class="faq-list-real">
+          <li>Menyediakan informasi penting seperti; Harga, Lokasi, dan Ketersediaan Spot</li>
+          <li> Memberikan akses User Dashboard untuk calon pengiklan dan agensi agar dapat mencari, memilih, dan menyewa billboard serta mengatur campaign iklan secara efisien dan cepat</li>
+          <li> Membantu memudahkan proses transaksi antara calon pengiklan dan agensi dengan pemilik billboard</li>
+        </ul>
+      </article>
+      <article id="1">
+        <h3><strong>Apa keunggulan Kiviads?</strong></h3>
+        <ul class="faq-list-real">
+          <li>Menyediakan informasi penting seperti; Harga, Lokasi, dan Ketersediaan Spot</li>
+          <li> Memberikan akses User Dashboard untuk calon pengiklan dan agensi agar dapat mencari, memilih, dan menyewa billboard serta mengatur campaign iklan secara efisien dan cepat</li>
+          <li> Membantu memudahkan proses transaksi antara calon pengiklan dan agensi dengan pemilik billboard</li>
+        </ul>
+      </article>
+      <article id="1">
+        <h3><strong>Apa keunggulan Kiviads?</strong></h3>
+        <ul class="faq-list-real">
+          <li>Menyediakan informasi penting seperti; Harga, Lokasi, dan Ketersediaan Spot</li>
+          <li> Memberikan akses User Dashboard untuk calon pengiklan dan agensi agar dapat mencari, memilih, dan menyewa billboard serta mengatur campaign iklan secara efisien dan cepat</li>
+          <li> Membantu memudahkan proses transaksi antara calon pengiklan dan agensi dengan pemilik billboard</li>
+        </ul>
+      </article>
+      <article id="1">
+        <h3><strong>Apa keunggulan Kiviads?</strong></h3>
+        <ul class="faq-list-real">
+          <li>Menyediakan informasi penting seperti; Harga, Lokasi, dan Ketersediaan Spot</li>
+          <li> Memberikan akses User Dashboard untuk calon pengiklan dan agensi agar dapat mencari, memilih, dan menyewa billboard serta mengatur campaign iklan secara efisien dan cepat</li>
+          <li> Membantu memudahkan proses transaksi antara calon pengiklan dan agensi dengan pemilik billboard</li>
+        </ul>
+      </article>
+      <article id="1">
+        <h3><strong>Apa keunggulan Kiviads?</strong></h3>
+        <ul class="faq-list-real">
+          <li>Menyediakan informasi penting seperti; Harga, Lokasi, dan Ketersediaan Spot</li>
+          <li> Memberikan akses User Dashboard untuk calon pengiklan dan agensi agar dapat mencari, memilih, dan menyewa billboard serta mengatur campaign iklan secara efisien dan cepat</li>
+          <li> Membantu memudahkan proses transaksi antara calon pengiklan dan agensi dengan pemilik billboard</li>
+        </ul>
+      </article>
+    </div>
+  </div>
+  <div class="col-md-4 sidebar">
+    <ul class="xoxo">
+
+      <li id="text-4" class="widget-container widget_text"><h3 class="widget-title">Wego Offices</h3>     <div class="textwidget"><div id="text-5">
+        <dl>
+          <dt>Wego Singapore</dt>
+          <dd>
+            <address>100 Beach Road<br>
+              #23-07/13 Shaw Tower<br>
+              Singapore 189702</address>
+              <a class="map" href="https://maps.google.com.sg/maps?ie=UTF8&amp;cid=12684447638230262860&amp;q=Wego+Pte+Ltd&amp;iwloc=A&amp;gl=SG&amp;hl=en" target="_blank">View in Google Maps</a>
+              <p class="phone">+65 6692 0868</p>
+            </dd>
+            <dt>Wego Indonesia</dt>
+            <dd>
+              <address>Menara Palma #10-01<br>
+                Jl. HR Rasuna Said<br>
+                Blok X2 - Kav. 6 Kuningan<br>
+                Jakarta, Indonesia</address>
+                <a class="map" href="https://maps.google.co.id/maps?ie=UTF8&amp;cid=12685516303996739440&amp;q=Wego+Indonesia&amp;iwloc=A&amp;gl=ID&amp;hl=en" target="_blank">View in Google Maps</a>
+                <p class="phone">+62 21 5795 7405</p>
+                <p class="fax">+62 21 5795 7406</p>
+              </dd>
+              <dt>Wego India</dt>
+              <dd>
+                <address>#3164, 2nd Floor,  “SS Konerira”<br>
+                  ESI-Domlur Service Road<br>
+                  HAL 2nd Stage, Indiranagar<br>
+                  Bangalore – 560008 India</address>
+                  <a class="map" href="https://maps.google.co.in/maps?ie=UTF8&amp;cid=10707668231065200708&amp;q=Wego+India&amp;iwloc=A&amp;gl=IN&amp;hl=en" target="_blank">View in Google Maps</a>
+                  <p class="phone">+91 80 4096 1098</p>
+                </dd>
+                <dt>Wego Middle East</dt>
+                <dd>
+                  <address>#02-213<br>
+                    Building 4<br> Dubai Internet City<br> Dubai, United Arab Emirates<br></address>
+                    <p class="phone">T: +9714 4219936</p>
+                    <p class="fax">F: +9714 4219439</p>
+                  </dd>
+                </dl>
+              </div></div>
+            </li>     </ul>
+          </div>
+        </div>
+        <?php $this->
+        renderPartial('/shared/partial/footer');?>
